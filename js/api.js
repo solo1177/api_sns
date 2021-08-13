41461d5a3065aa1fac64df71588d5af496ab7fd8
solo <deskspace0716@gmail.com>
2021-08-13 16:38:52 +0900

    codefix

--- js/api.js
+++ js/api.js
@@ -1,14 +1,7 @@
-
-function makeBasicAuth(userid, userpw) {
-  var token = userid + ':' + userpw;
-  var hash = btoa(token);
-  return "Basic " + hash;
-}
-
-function getextAPI(url, method, callback) {
+function gettextAPI(url, method, callback) {
   var xhr = new XMLHttpRequest();
   //xhr.open(【どんな方法でデータを取得するか】, 【どのサーバーに対して】)
-  xhr.send();
+  //xhr.send();
   xhr.onreadystatechange = function() {
     //if(【いつデータを取得するか】) 
     if(xhr.readyState === 4 && xhr.status === 200) {  //データ取得後の処理内容 （通信が成功した時）
@@ -17,17 +10,17 @@ function getextAPI(url, method, callback) {
   }
 
   $.ajax({
-      url: url, //アクセスするURLかディレクトリ
-      type: method,
+      url: url + "text/all", //アクセスするURLかディレクトリ
+      type: method, //getかput
       cache: false, //cacheを使うかどうか
       dataType:'json', //data type scriptなどデータタイプの指定
-      data:data, //アクセスするときに必要なデータを 記載
 
-      beforeSend: function (xhr) {
-        xhr.setRequestHeader('Authorization', makeBasicAuth(userid, userpw));
-      },
-	    
-      success : function(data, status, xhr) {//通信が成功したときのコールバックの処理を書く
+      success : function() {//通信が成功したときのコールバックの処理を書く
+        callback({//コールバックする内容を返す
+          status : 'success',
+          description : 'Succeeded',
+        });
+
         var retrievedSensors = {};
         var results = {};
         for (i=0; i<data.length; i++) {
@@ -39,16 +32,20 @@ function getextAPI(url, method, callback) {
           } else {
             results[type] = retrievedSensors[type];
           }
-      });
-
-      callback({//コールバックする内容を返す
+        });
+        callback({
           status : 'success',
           description : 'Succeeded getting room status',
           illuminance : results.illuminance,
-          });
+          humidity : results.humidity,
+          airpressure : results.airpressure,
+          temperature : results.temperature
+        });
+        
+
 	    },
 
-	    error : function(xhr, status, error) {  //通信が失敗したときのコールバックの処理を書く
+	    error : function(xhr) {  //通信が失敗したときのコールバックの処理を書く
         var err = JSON.parse(xhr.responseText);
         var errorMsg = '[' + err.status + '] '  + err.description ;
         callback({
--- js/sns.js
+++ js/sns.js
@@ -1,4 +1,4 @@
-var BASE_URL = "https://versatileapi.herokuapp.com/api";
+var BASE_URL = "https://versatileapi.herokuapp.com/api/";
 
 function displaytext(result) {
 
@@ -7,32 +7,16 @@ function displaytext(result) {
       var monitoringResults = document.getElementById("monitoring-result")
       //  status: API呼出結果（成功時は”success”、失敗時は”fail”）
       monitoringResults.textContent = String("success")
-      //   temperature: 気温
-      var temperature = document.getElementById("room-temperature")
-      //  description: API呼出結果の詳細
-      temperature.textContent = result.temperature
   } 
   else {
-    var monitoringResults = document.getElementById("monitoring-result")
-    monitoringResults.textContent = String("none")
-  
     // 失敗の場合： none
-    var temperature = document.getElementById("room-temperature")
-
-    temperature.textContent = String("none")
+    var monitoringResults = document.getElementById("monitoring-result")
+    monitoringResults.textContent = String("error")
   }
 }
 
 function gettext() {
-   //userid
-   var u = document.getElementById("iniad-id");
-   var userid = u.value
-   //userpw 
-   var up = document.getElementById("iniad-pw");
-   var userpw = up.value
-
    //url
    var url = BASE_URL;  
-
   gettextAPI(url, 'GET', displaytext);
 }
