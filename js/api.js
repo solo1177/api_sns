68b09a525367eb99c605a265f90a64d5cec36c0c
solo <deskspace0716@gmail.com>
2021-08-14 16:30:38 +0900

    add-display-text

--- js/api.js
+++ js/api.js
@@ -15,20 +15,28 @@ function gettextAPI(url, method, callback) {
       cache: false, //cacheを使うかどうか
       dataType:'json', //data type scriptなどデータタイプの指定
 
-      success : function(data) {//通信が成功したときのコールバックの処理を書く
+      //通信が成功したときのコールバックの処理を書く
+      success : function(data) {
         //取得したデータを入れる
-        var results = "";
+        var text = [];
+        var user_id = [];
+        var updated_at = [];
         for (i=0; i<data.length; i++) {
-          results += data[i].text + "\n";
+          text.push(data[i].text);
+          user_id.push(data[i]._user_id);
+          updated_at.push(data[i]._updated_at);
         }
-        
+      
         callback({//コールバックする内容を返す
           status : 'success',//ステータス
-          text : results
+          text : text,
+          user_id : user_id,
+          updated_at : updated_at
         });
 	    },
-
-	    error : function(xhr) {  //通信が失敗したときのコールバックの処理を書く
+      
+      //通信が失敗したときのコールバックの処理を書く
+	    error : function(xhr) {  
         var err = JSON.parse(xhr.responseText);
         var errorMsg = '[' + err.status + '] '  + err.description ;
         callback({
@@ -36,5 +44,6 @@ function gettextAPI(url, method, callback) {
           description : errorMsg,
         }
       )}
+      
     })
 }
--- js/sns.js
+++ js/sns.js
@@ -6,10 +6,30 @@ function displaytext(result) {
       var monitoringResults = document.getElementById("monitoring-result")
       //  status: API呼出結果（成功時は”success”、失敗時は”fail”）
       monitoringResults.textContent = String("success")
-      
-      //つぶやきの内容
-      var text = document.getElementById("return-text")
-      text.textContent = result.text
+    
+      //つぶやきの内容を一つずつ取得。pタグとdivタグで囲んで返す
+      for (i=0; i<result.text.length; i++) {  
+        var textdiv = document.getElementById("textdiv");
+        
+        //user_id
+        var user_p = document.createElement("p");
+        var user_id = document.createTextNode(result.user_id[i])
+        user_p.appendChild(user_id);
+        textdiv.appendChild(user_p);
+
+        //posted_at
+        var posted_p = document.createElement("p");
+        var updated_at = document.createTextNode(result.updated_at[i])
+        posted_p.appendChild(updated_at)
+        textdiv.appendChild(posted_p);
+
+        //text
+        var text_p = document.createElement("p");        
+        var text = document.createTextNode(result.text[i])
+        text_p.appendChild(text);
+        textdiv.appendChild(text_p);
+      }
+
   } 
   else {
     // 失敗の場合： none
