03d9c666d197b56c4b25db41d5cbce5902d22ee5
solo <deskspace0716@gmail.com>
2021-08-14 17:02:18 +0900

    add-text-div

--- js/sns.js
+++ js/sns.js
@@ -8,14 +8,17 @@ function displaytext(result) {
       monitoringResults.textContent = String("success")
     
       //つぶやきの内容を一つずつ取得。pタグとdivタグで囲んで返す
-      for (i=0; i<result.text.length; i++) {  
-        var textdiv = document.getElementById("textdiv");
-        
+      var resultdiv = document.getElementById("result");
+
+      for (i=0; i<result.text.length; i++) { 
+        var textdiv = document.createElement("div"); //divタグを作る
+        textdiv.className ="text_all"
+
         //user_id
-        var user_p = document.createElement("p");
-        var user_id = document.createTextNode(result.user_id[i])
-        user_p.appendChild(user_id);
-        textdiv.appendChild(user_p);
+        var user_p = document.createElement("p"); //pタグを作る
+        var user_id = document.createTextNode(result.user_id[i]) //user_idを文字情報として取得
+        user_p.appendChild(user_id); //user_idをpタグの中に追加
+        textdiv.appendChild(user_p); //pタグをdivに追加
 
         //posted_at
         var posted_p = document.createElement("p");
@@ -28,6 +31,9 @@ function displaytext(result) {
         var text = document.createTextNode(result.text[i])
         text_p.appendChild(text);
         textdiv.appendChild(text_p);
+        console.log(textdiv)
+
+        resultdiv.appendChild(textdiv);
       }
 
   } 
